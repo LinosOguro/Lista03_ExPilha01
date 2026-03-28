b428dc1771c0ebd5c0ceee3c7abdde772cb4fb93
LinosOguro <93005894+LinosOguro@users.noreply.github.com>
2026-03-28 17:58:35 -0300

    Update Principal.java

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -29,25 +29,3 @@ public class Principal {
 		System.out.println("Quantidade: " + p.Size());
 	}
 }
-
-/*
-PilhaInt p = new PilhaInt();
-
-		int[] vetor = { 0, 5, 7, -4, 3, 5, -2, -1, 10, 4, 3, -6, 2, -9, 1, -5 };
-
-		for (int i = 0; i < vetor.length; i++) {
-			if (vetor[i] >= 0) {
-				System.out.println("Push: " + vetor[i]);
-				p.Push(vetor[i]);
-			} else {
-
-				int v1 = p.Pop();
-				int v2 = p.Pop();
-				System.out.println("Pop: " + v1 + "\nPop: " + v2);
-				int soma = v1 + v2;
-				p.Push(vetor[i]);
-				p.Push(soma);
-			}
-		}
-		System.out.println("Total de valores: " + p.Size());
-*/
